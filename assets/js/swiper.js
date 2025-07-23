1c788637108488c92a125eb1930b049209a9f5fc
Subhan Ahmed <subhanahmede860@gmail.com>
2025-07-23 07:13:04 +0500

    Computer Playz Website

---
+++ assets/js/swiper.js
@@ -0,0 +1,45 @@
+// Banner Slider
+const bannerSwiper = new Swiper(".banner", {
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+});
+
+const categorySwiper = new Swiper(".categorySlide", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+});
+
+const productSwiper = new Swiper(".productSlide", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+});
+
+
